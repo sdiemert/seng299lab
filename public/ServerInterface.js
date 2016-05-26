cac8deadf51accf5cf9a3e17d9e49b72f7be2d9a
Simon Diemert <simon.diemert@gmail.com>
2016-05-25 18:22:14 -0700

    timer loop works

---
+++ public/ServerInterface.js
@@ -0,0 +1,75 @@
+"use strict"
+/**
+ * Created by sdiemert on 2016-05-25.
+ */
+
+class ServerInterface{
+
+    constructor(url, port){
+        this._url = url || "localhost"; 
+        this._port = port || 3000; 
+    }
+
+    /**
+     * @param obj {object} the data to send.
+     * @param path {string} the server path to make the request to.
+     * @param callback {function} called when the server responds.
+     *      Takes 1 parameter, an error parameter which is null if everything is OK.
+     */
+    _sendData(obj, path, callback){
+
+        var u = this._url +":"+ this._port + path;
+
+        console.log("sending POST to "+u);
+
+        var obj = { number : 10};
+        var postXhr = new XMLHttpRequest();
+        postXhr.open("POST", u, true);
+        postXhr.setRequestHeader("Content-type", "application/json");
+        postXhr.send(JSON.stringify(obj));
+
+        postXhr.onreadystatechange = function(){
+
+            // this function is executed when the request comes 
+            // back from the server. 
+
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                callback(null);
+            }else if(xhr.readyState == 4 && xhr.status !== 200){
+                callback(xhr.status);
+            }
+        }
+    }
+
+    /**
+     * Requests time tracker data from the server using a HTTP GET request.
+     *
+     * @param callback {function} the function to call when the get request comes back.
+     *      takes 2 parameters:
+     *          1) an error paramenter, will be null if everything was OK.
+     *          2) an object that represents the response from the server.
+     */
+     getData(callback) {
+        console.log("sending GET to /data");
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/data", true);
+        xhr.send();
+
+        xhr.onreadystatechange = function () {
+
+            // this function is executed when the request comes
+            // back from the server.
+
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                callback(null, JSON.parse(xhr.responseText));
+            }else if(xhr.readyState == 4 && xhr.status !== 200){
+                callback(xhr.status, null);
+            }
+        };
+    }
+
+
+
+
+}
--- public/myScript.js
+++ public/myScript.js
@@ -1,24 +1,67 @@
+// FILE: public/myScript.js
 
-// FILE: public/myScript.js 
+//keep track of the current timer task.
+var currentTask = null;
 
+/**
+ * Starts the timer for the current task.
+ */
+function startTimer(){
 
-var xhr = new XMLHttpRequest(); 
-xhr.open("GET", "/data", true);
-xhr.send(); 
+    var name = document.getElementById("task-name").value;
+    var proj = document.getElementById("project").value;
+    
+    if(!name || !proj){
+        
+        console.log("No task or project!");
+        alert("Please enter a task and a project!");
+        
+    }else{
+        
+        currentTask = {}; 
+        currentTask.name = name; 
+        currentTask.project = project; 
+        currentTask.startTime = new Date(); 
+        currentTask.endTime = null; 
+        
+        document.getElementById("start-button").disabled = true;
+        document.getElementById("stop-button").disabled = false;
 
-xhr.onreadystatechange = function(){
-    if(xhr.readyState == 4 && xhr.status == 200){
-        // we got a response! 
-        console.log(xhr.responseText);
-        document.getElementById("response").innerHTML = xhr.responseText;
+        timerLoop();
+
     }
-};
 
-function sendData(){
-    console.log("sending POST to /data");
-    var obj = { number : 10};
-    var postXhr = new XMLHttpRequest(); 
-    postXhr.open("POST", "/data", true);
-    postXhr.setRequestHeader("Content-type", "application/json");
-    postXhr.send(JSON.stringify(obj));
 }
+
+/**
+ * Shows the current timer ticking.
+ */
+function timerLoop(){
+    setTimeout(function(){
+       if(currentTask){
+           var d = new Date(); 
+           var secs = Math.floor((d - currentTask.startTime)/1000);
+           var mins = Math.floor(secs/60);
+           var s = (mins >= 10) ? mins+":" : "0"+mins+":";
+           s += (secs%60 >= 10) ? secs%60 : "0"+secs%60;
+           document.getElementById("timer-time").innerHTML = s;
+           
+           timerLoop()
+       }
+    }, 1000);
+}
+
+function stopTimer(){
+
+    document.getElementById("start-button").disabled = false;
+    currentTask = null;
+
+}
+
+function rebuildTable(object){
+
+    
+
+}
+
+
